70851c7e7772e1c30415881ddbeb543d06674b58
Ivana505 <ivana.iles@protonmail.com>
2022-07-15 14:05:26 +0000

    Fix links to register/login

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-#from shop.models import Chocolate
+from shop.models import Chocolate
 
 
 
 # Create your views here.
 def index(request):
     """ return to the index page """
-    chocolates = Chocolate.objects.all() 
+    chocolates = Chocolate.objects.all()
     return render(request, 'home/index.html', {'chocolates': chocolates})
