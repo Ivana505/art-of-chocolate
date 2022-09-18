6cfd824bb18c2a9daa4435774a2d1fd28d0d3d33
Ivana505 <ivana.iles@protonmail.com>
2022-09-18 00:45:36 +0000

    Add @property for images to avoid error when no image uploaded

--- art_of_chocolate/settings.py
+++ art_of_chocolate/settings.py
@@ -135,13 +135,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 
--- shop/models.py
+++ shop/models.py
@@ -15,10 +15,22 @@ class Chocolate(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
 
 
     def __str__(self):
         return self.name
+
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 
 
 class Order(models.Model):
