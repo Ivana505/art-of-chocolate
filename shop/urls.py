0dbcb70c14626e4a9832b92a95643ae745f38dbe
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-09-29 23:20:04 +0000

    Remove uncommented paths

--- shop/urls.py
+++ shop/urls.py
@@ -5,13 +5,10 @@ urlpatterns = [
     path('', views.shop, name="shop"),
     path('basket/', views.basket, name="basket"),
     path('checkout/', views.checkout, name="checkout"),
-    #path('payment/', views.payment, name="payment"),
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
-    #path('', views.home, name="home"),
     path('chocolate_page/<str:id>', views.chocolate_page, name="chocolate_page"),
     path('delete_chocolate/<pk>', views.DeleteProductView.as_view(), name="delete_chocolate"),
     path('add_chocolate/', views.add_product, name="add_chocolate"),
     path('edit_chocolate/<pk>', views.edit_product, name="edit_chocolate"),
-    #path('create-checkout-session', views.CreateCheckoutSessionView, name='create-checkout-session')
 ]
