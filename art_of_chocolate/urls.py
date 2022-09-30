8bd449b2bd0c3725687f575582515f864a0f42ba
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-09-30 09:45:24 +0000

    Correct url path

--- art_of_chocolate/urls.py
+++ art_of_chocolate/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop', include('shop.urls')),
+    path('', include('shop.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
