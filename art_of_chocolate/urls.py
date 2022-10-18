aa10a0a7e401287cac16c808a9d8dd7406fd26da
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-10-18 16:05:34 +0000

    Add 404 custom page by adding new url.py in art_of_chocolate app and adding errors folder

--- art_of_chocolate/urls.py
+++ art_of_chocolate/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,6 @@ urlpatterns = [
     path('marketing/', include('marketing.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+
+handler404 = 'art_of_chocolate.views.handler404'
