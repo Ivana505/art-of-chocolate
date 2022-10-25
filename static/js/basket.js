37cd86c2c2cd9a186a9f764f1f74c0d21c503505
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-10-25 15:07:03 +0000

    Add semicolons to basket.js to correct the code

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,25 +1,26 @@
 /* jshint esversion: 11, jquery: true */
-var updateBtns = document.getElementsByClassName('update-basket')
+
+var updateBtns = document.getElementsByClassName('update-basket');
 
 for (var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function () {
         var chocolateId = this.dataset.chocolate;
         var action = this.dataset.action;
-        console.log('chocolateId:', chocolateId, 'action:', action)
+        console.log('chocolateId:', chocolateId, 'action:', action);
 
         console.log('USER', user);
         if (user == 'AnonymousUser') {
-            addCookieItem(chocolateId, action)
+            addCookieItem(chocolateId, action);
         }else {
-            updateUserOrder(chocolateId, action)
+            updateUserOrder(chocolateId, action);
         }
-    })
+    });
 }
 
 function updateUserOrder(chocolateId, action) {
-    console.log('You are logged in, processing data...')
+    console.log('You are logged in, processing data...');
 
-    var url = '/update_item/'
+    var url = '/update_item/';
 
     fetch(url, {
             method: 'POST',
@@ -34,11 +35,11 @@ function updateUserOrder(chocolateId, action) {
         })
 
     .then((response) => {
-            return response.json()
+            return response.json();
         })
     .then((data) => {
-            console.log('data', data)
-            location.reload()
-        })
+            console.log('data', data);
+            location.reload();
+        });
 
 }
