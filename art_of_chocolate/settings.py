f1046b82c24a964a1e0a29890b7a28dbb0b59cf5
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-11-02 19:57:25 +0000

    Change debug to False in the env

--- art_of_chocolate/settings.py
+++ art_of_chocolate/settings.py
@@ -6,7 +6,7 @@ if os.path.isfile('env.py'):
 
 import environ
 env = environ.Env(
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 environ.Env.read_env()
 
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
-DEBUG = os.environ.get("DEBUG", True)
+# DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = ['art-of-chocolate.herokuapp.com', 'localhost']
 
