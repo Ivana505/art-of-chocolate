0a85f92de24cd7e08f3869260607558d18c7335f
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-11-03 13:42:07 +0000

    Remove everything related to Resized Image package

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from cloudinary.models import CloudinaryField
-from django_resized import ResizedImageField
 
 
 class Buyer(models.Model):
