f6b621f483c8e3a72df999eee3e8af032b2ac714
Ivana505 <ivana.iles@protonmail.comgit config user.email ivana.iles@protonmail.com>
2022-11-06 15:47:13 +0000

    Remove debug

--- art_of_chocolate/settings.py
+++ art_of_chocolate/settings.py
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
-DEBUG = os.environ.get("DEBUG", True)
+#DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = ['art-of-chocolate.herokuapp.com', 'localhost']
 
